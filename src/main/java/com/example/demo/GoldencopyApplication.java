d48c24b42a0eec9ae75088b8d589c313d06d5704
16033 <16033@LAPTOP-N2HE1M2J.lan>
2020-08-23 17:39:18 -0500

    Initial Commit

---
+++ src/main/java/com/example/demo/GoldencopyApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldencopyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoldencopyApplication.class, args);
+	}
+
+}
