a9d86115b5cfb4b3eee0bdb93bd6991d837aa004
Anjanirak <anjanikumarr2008@gmail.com>
2025-10-09 22:39:26 -0500

    Added the comment

--- src/main/java/com/example/controller/goldencopyController.java
+++ src/main/java/com/example/controller/goldencopyController.java
@@ -6,7 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class goldencopyController {
-	
+
+	// Added the comments
 	@GetMapping("/getName")
 	public String getName(@RequestParam(name = "userName") String name)
 	{
