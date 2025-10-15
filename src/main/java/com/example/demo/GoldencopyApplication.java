e9faa7e521055c112a51d38b756575fb8b180782
Anjanirak <anjanikumarr2008@gmail.com>
2025-10-15 00:24:22 -0500

    World3 commit

--- src/main/java/com/example/demo/GoldencopyApplication.java
+++ src/main/java/com/example/demo/GoldencopyApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GoldencopyApplication {
 
 	public static void main(String[] args) {
+	    System.out.println("Hello World Before");
 		SpringApplication.run(GoldencopyApplication.class, args);
 		System.out.println("Hello World");
 		System.out.println("Hello World2");
